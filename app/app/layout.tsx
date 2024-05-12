ab5edc8a2f002e53201f35296aa562e73b51804a
alex-js-ltd <alex.js.ltd@gmail.com>
2024-05-12 17:53:43 +0200

    update layout

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -32,35 +32,36 @@ export default function RootLayout({
 		</html>
 	)
 }
+function Background({ children }: { children: ReactNode }) {
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			<div className="relative mb-4 flex items-center justify-center py-[26vh] pt-[18vh] text-gray-900 sm:pt-[26vh] w-full">
+				<div className="absolute inset-0 flex items-center justify-center overflow-hidden">
+					<div className="relative mb-72 h-full w-full min-w-[29rem] max-w-[96rem] sm:mb-0">
+						<Image
+							className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+							src="/background.svg"
+							alt="background"
+							fill={true}
+							priority
+						/>
+					</div>
+				</div>
 
-const Background = ({ children }: { children: ReactNode }) => (
-	<main className="flex min-h-screen flex-col items-center justify-between p-24">
-		<div className="relative mb-4 flex items-center justify-center py-[26vh] pt-[18vh] text-gray-900 sm:pt-[26vh] w-full">
-			<div className="absolute inset-0 flex items-center justify-center overflow-hidden">
-				<div className="relative mb-72 h-full w-full min-w-[29rem] max-w-[96rem] sm:mb-0">
-					<Image
-						className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-						src="/background.svg"
-						alt="background"
-						fill={true}
-						priority
-					/>
+				<div className="relative flex w-full flex-col items-center gap-6 px-6 text-center">
+					<div className="flex w-full flex-col items-center gap-1.5">
+						<h2
+							className="text-4xl font-semibold tracking-tighter sm:text-5xl @media(max-width:480px):text-[2rem]"
+							data-testid="home-h2"
+						>
+							Generate. Mint. Deploy.
+						</h2>
+					</div>
+					<div className="z-10 m-auto flex w-full flex-col divide-zinc-600 overflow-hidden rounded-xl bg-gray-900 shadow-lg shadow-black/40 sm:max-w-xl">
+						{children}
+					</div>
 				</div>
 			</div>
-
-			<div className="relative flex w-full flex-col items-center gap-6 px-6 text-center">
-				<div className="flex w-full flex-col items-center gap-1.5">
-					<h2
-						className="text-4xl font-semibold tracking-tighter sm:text-5xl @media(max-width:480px):text-[2rem]"
-						data-testid="home-h2"
-					>
-						Generate. Mint. Deploy.
-					</h2>
-				</div>
-				<div className="z-10 m-auto flex w-full flex-col divide-zinc-600 overflow-hidden rounded-xl bg-gray-900 shadow-lg shadow-black/40 sm:max-w-xl">
-					{children}
-				</div>
-			</div>
-		</div>
-	</main>
-)
+		</main>
+	)
+}
