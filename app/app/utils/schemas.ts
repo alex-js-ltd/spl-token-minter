71aacfc956c0449b85cd9f24fccdeb4fe2db76eb
alex-js-ltd <alex.js.ltd@gmail.com>
2024-05-13 22:30:46 +0100

    update

--- app/app/utils/schemas.ts
+++ app/app/utils/schemas.ts
@@ -6,3 +6,16 @@ export const MetaData = z.object({
 	decimals: z.number(),
 	uri: z.string(),
 })
+
+const MAX_UPLOAD_SIZE = 1024 * 1024 * 3 // 3MB
+
+export const ImageFieldsetSchema = z.object({
+	id: z.string().optional(),
+	file: z
+		.instanceof(File)
+		.optional()
+		.refine(file => {
+			return !file || file.size <= MAX_UPLOAD_SIZE
+		}, 'File size must be less than 3MB'),
+	altText: z.string().optional(),
+})
