220f39078233717731226859483258884aa2f815
alex-js-ltd <alex.js.ltd@gmail.com>
2024-05-13 17:51:46 +0200

    remove err bound

--- app/app/error.tsx
+++
@@ -1,9 +0,0 @@
-'use client' // Error components must be Client Components
-
-import ErrorBoundary from '@/app/comps/error_boundary'
-
-export default function Error(
-	props: React.ComponentProps<typeof ErrorBoundary>,
-) {
-	return <ErrorBoundary {...props} />
-}
