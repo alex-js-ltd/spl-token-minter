3defe6a8c139524f64aad9c2daab05a7ec6a9cfb
alex-js-ltd <alex.js.ltd@gmail.com>
2024-05-15 19:47:05 +0100

    add prisma

--- app/app/api/upload/route.ts
+++ app/app/api/upload/route.ts
@@ -5,7 +5,6 @@ export async function POST(request: Request): Promise<NextResponse> {
 	const { searchParams } = new URL(request.url)
 	const filename = searchParams.get('filename')
 
-	console.log('filename', filename)
 	if (!filename || !request.body) return NextResponse.json({})
 
 	const blob = await put(filename, request.body, {
