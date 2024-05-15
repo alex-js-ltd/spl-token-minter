a9a0624c6294f36b48dd2b07305c060ef0e68892
alex-js-ltd <alex.js.ltd@gmail.com>
2024-05-15 20:53:48 +0100

    update the metadata

---
+++ app/app/api/metadata/[id]/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from 'next/server'
+import { prisma } from '@/app/utils/db'
+
+export async function GET(
+	_request: Request,
+	{ params }: { params: { id: string } },
+): Promise<NextResponse> {
+	if (!params.id) return NextResponse.json({})
+
+	const token = await prisma.tokenMetaData.findUnique({
+		where: {
+			id: params.id,
+		},
+	})
+
+	if (!token) return NextResponse.json({})
+
+	const { id, ...rest } = token
+
+	return NextResponse.json({ ...rest })
+}
