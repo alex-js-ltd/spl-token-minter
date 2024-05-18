4a1b4fad676ad6d6a5b47a686f7468cb2cbec0f0
alex-js-ltd <alex.js.ltd@gmail.com>
2024-05-18 22:01:13 +0100

    update

--- app/app/comps/input.tsx
+++ app/app/comps/input.tsx
@@ -3,7 +3,7 @@ import { cva, type VariantProps } from 'class-variance-authority'
 import { cn } from '@/app/utils/misc'
 
 const inputVariants = cva(
-	'min-h-[1.5rem] w-full flex-[1_0_50%] resize-none border-0 bg-transparent pr-2 text-sm leading-relaxed text-white shadow-none outline-none ring-0 [scroll-padding-block:0.75rem] selection:bg-teal-300 selection:text-black placeholder:text-zinc-400 disabled:bg-transparent disabled:opacity-80 [&amp;_textarea]:px-0',
+	'min-h-[1.5rem] w-full flex-[1_0_50%] resize-none border-0 bg-gray-900 pr-2 text-sm leading-relaxed text-white shadow-none outline-none ring-0 [scroll-padding-block:0.75rem] selection:bg-teal-300 selection:text-black placeholder:text-zinc-400 disabled:bg-transparent disabled:opacity-80 [&amp;_textarea]:px-0',
 	{
 		variants: {
 			variant: {
