422f24ba3f4023ac89f93e12bf56a56f50fe7590
alex-js-ltd <alex.js.ltd@gmail.com>
2024-05-22 01:59:41 +0100

    update

--- app/app/comps/field.tsx
+++ app/app/comps/field.tsx
@@ -1,5 +1,5 @@
 import { Input, type InputProps } from './input'
-import { cn } from '../utils/misc'
+import { cn } from '@/app/utils/misc'
 
 export type ListOfErrors = Array<string | null | undefined> | null | undefined
 
@@ -14,17 +14,16 @@ export function Field({
 	const errorId = errors?.length ? `${id}-error` : undefined
 
 	return (
-		<div className={cn('w-full h-[69px]', inputProps.className)}>
+		<div className={cn('w-full h-[69px] relative', inputProps.className)}>
+			{errors?.map(el => (
+				<div
+					key={el}
+					className="text-teal-300 text-xs absolute -top-3 left-3 flex"
+				>
+					{el}
+				</div>
+			))}
 			<div className="relative flex w-full gap-2 border-b border-opacity-[0.125] p-3 transition duration-500 ease-in-out border-white">
-				{errors?.map(el => (
-					<div
-						key={el}
-						className="text-teal-300 text-xs absolute -top-3 left-3 flex gap-1"
-					>
-						{el}
-					</div>
-				))}
-
 				<Input
 					id={id}
 					aria-invalid={errorId ? true : undefined}
