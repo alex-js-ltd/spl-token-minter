97a1c6e0c22cb1e2f6109f30f0f4f10fe4efae86
alex-js-ltd <alex.js.ltd@gmail.com>
2024-05-28 23:10:19 +0200

    add use get mint

--- app/app/comps/mint_button.tsx
+++ app/app/comps/mint_button.tsx
@@ -3,6 +3,7 @@
 import { Keypair } from '@solana/web3.js'
 import { useMintSomeTokens } from '@/app/hooks/use_mint_some_tokens'
 import { useRevokeMintAuth } from '@/app/hooks/use_revoke_mint_auth'
+import { useGetMint } from '../hooks/use_get_mint'
 import { Icon } from './_icon'
 
 type MintButtonProps = {
@@ -16,6 +17,8 @@ export function MintButton({ mintKeypair, symbol, supply }: MintButtonProps) {
 
 	const { revokeMintAuth } = useRevokeMintAuth({ mintKeypair })
 
+	const getMint = useGetMint({ mintKeypair })
+
 	return (
 		<>
 			<button
@@ -23,6 +26,9 @@ export function MintButton({ mintKeypair, symbol, supply }: MintButtonProps) {
 				onClick={async () => {
 					await run(mintSomeTokens(supply))
 					await run(revokeMintAuth())
+
+					const res = await run(getMint())
+					console.log(res)
 				}}
 				className="ml-auto inline-flex select-none items-center gap-1 whitespace-nowrap rounded-full border border-zinc-200 bg-white px-2 py-0.5 transition-colors hover:border-zinc-800"
 			>
